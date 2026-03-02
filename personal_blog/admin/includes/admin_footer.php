fc7e84682c8f2767db83a2ce45a4367a089d51c0
azrofx-code <azrofxpert@gmail.com>
2026-03-03 01:03:58 +0600

    Add files via upload

--- personal_blog/admin/includes/admin_footer.php
+++ personal_blog/admin/includes/admin_footer.php
@@ -1,13 +1,14 @@
 </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
+    <script src="https://cdn.ckeditor.com/ckeditor5/39.0.1/classic/ckeditor.js"></script>
     <script>
-        tinymce.init({
-            selector: 'textarea[name="post_content"]',
-            plugins: 'lists link preview',
-            toolbar: 'undo redo | formatselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link',
-            menubar: false
-        });
+        if (document.querySelector('textarea[name="post_content"]')) {
+            ClassicEditor
+                .create(document.querySelector('textarea[name="post_content"]'))
+                .catch(error => {
+                    console.error(error);
+                });
+        }
     </script>
 </body>
 </html>
